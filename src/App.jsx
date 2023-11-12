9f9a593c3b3a6dd91ad6c35f71fb969232f65ed7
ewi josepha <ewijosepha@gmail.com>
2023-11-12 16:14:24 +0100

    adding popular categories

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './Components/Navbar/Navbar'
+import Landingimage from './Components/Landingpage/Landingimage'
 
 function App() {
 
   return (
     <>
-  <h1>hello</h1>
+    <div id="app">
+   
+    <Navbar />
+    <Landingimage />
+  
+
+    </div>
+  
     </>
   )
 }
